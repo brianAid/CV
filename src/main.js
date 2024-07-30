da9b79f54cc1a14cc1907aa383e63d57c63c4a5e
brian <brianadi1230@gmail.com>
2024-07-30 13:55:30 +0700

    commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const body = document.body;
+console.log(body.id);
+(localStorage.getItem("background")?body.id =localStorage.getItem('background'):localStorage.setItem("background","light"));
+
+function changeBg(){
+  switch (localStorage.getItem("background")) {
+    case "light":
+    localStorage.setItem("background","dark");
+    body.id = localStorage.getItem("background");
+      break;
+    case "dark":
+    localStorage.setItem("background","light");
+    body.id = localStorage.getItem("background");
+      break;
+  }
+}
